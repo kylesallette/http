4f7c9980fe57f222109d82f285314528897ba30b
kylesallette <kylesallette@gmail.com>
2017-12-19 18:34:14 -0700

    trying to figure out how to test this

--- test/server_test.rb
+++ test/server_test.rb
@@ -6,9 +6,15 @@ require './lib/server.rb'
 
 class ServerTest < Minitest::Test
 
-  def test_path
+  def test_it_exists
+    server = Server.new
 
+    assert_instance_of Server, server
   end
 
-
-end
+  def test_something
+    conn = Faraday.new(url: "localhost:9292")
+    server = Server.new
+    expected = conn.get/
+     assert_equal expected server.headers
+  end

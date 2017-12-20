469ae04f32846a1d82282086058d95817b9f4051
kylesallette <kylesallette@gmail.com>
2017-12-20 13:11:30 -0700

    cleaned up word search

--- lib/word_search.rb
+++ lib/word_search.rb
@@ -12,10 +12,10 @@ class WordSearch
         dic_words << words.chomp
       end
       if dic_words.include?(word)
-        "#{word.capitalize} is a known word"
+        "#{word.capitalize} is a known word."
       else
-        "#{word.capitalize} is not a known word"
+        "#{word.capitalize} is not a known word."
       end
-    end
+  end
 
 end

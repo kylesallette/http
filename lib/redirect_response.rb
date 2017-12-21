bfc750bc942ed3eb0caf405e534b503fa42b470f
kylesallette <kylesallette@gmail.com>
2017-12-20 17:44:48 -0700

    fixed redirect message

--- lib/redirect_response.rb
+++ lib/redirect_response.rb
@@ -1,24 +1,17 @@
 class RedirectResponse
 
 
-
-
-
   def redirect_message(request_lines, client)
     host = request_lines[1].split(":")[1]
     output = "<html><head></head><body></body></html>"
-    headers = [
-    "http/1.1 302 found",
-    "Location: #{host}:9292/game",
-    "date: #{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}",
-    "server: ruby",
-    "content-type: text/html; charset=iso-8859-1",
-    "content-length: #{output.length}\r\n\r\n"].join("\r\n")
-    headers
+    "<pre>
+    http/1.1 302 found
+    Location: #{host}:9292/game
+    date: #{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}
+    server: ruby
+    content-type: text/html; charset=iso-8859-1
+    content-length: #{output.length}
+    </pre>"
   end
-
-
-
-
 
 end
